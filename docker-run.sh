b4f33ff68171f7de18f191cce8e56352fb25314b
RuTel <rvelvetby@gmail.com>
2017-03-15 20:22:43 +0300

    Fix

--- docker-run.sh
+++ docker-run.sh
@@ -3,5 +3,5 @@
 cd /var/www/html && ./build.sh && /install.sh
 echo "extension=browserext.so" >> /etc/php/5.6/apache2/php.ini
 echo "export DOCUMENT_ROOT=html" >> /etc/apache2/envvars
-echo "export HOST=localhost" >> /etc/apache2/envvars
+echo "export HOSTNAME=localhost" >> /etc/apache2/envvars
 service apache2 restart
